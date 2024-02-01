d731a418d8279f9c6b912b0ab0805da0a331a830
rakibulapon46 <134295199+rakibulapon46@users.noreply.github.com>
2024-02-02 02:56:00 +0600

    done

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// initial value
+let count = 0;
+
+// select value and btns
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.target.classList;
+        if (styles.contains('decrease')) {
+            count--;
+        }
+        else if (styles.contains('increase')) {
+            count++;
+        }
+         else {
+            count = 0;
+        }
+        if(count > 0) {
+            value.style.color = 'green';
+        }
+        if(count < 0) {
+            value.style.color = 'red';
+        }
+        if(count === 0) {
+            value.style.color = '#000000';
+        }
+        value.textContent = count;
+    })
+});
